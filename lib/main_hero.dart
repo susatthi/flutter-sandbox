0f7d34ea7a61c78757da35c6a56d074f27cc6bec
susa.keyber <susa@keyber.jp>
2022-05-09 05:58:58 +0900

    ✨ feat: Heroのサンプル実装
    # ==================== Emojis & Prefix ====================
    # ✨ feat: 新しい機能
    # 🐛 fix: バグの修正
    # 📚 docs: ドキュメントのみの変更
    # 🖋 style: 空白、フォーマット、セミコロン追加など
    # 🧹 refactor: 仕様に影響がないコード改善(リファクタ)
    # 🐎 perf: パフォーマンス向上関連
    # 🚨 test: テスト関連
    # 🔧 chore: ビルド、補助ツール、ライブラリ関連
    # 🔖 version: バージョンタグ

---
+++ lib/main_hero.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Hero(
+              tag: 'back-button',
+              child: Material(
+                type: MaterialType.transparency,
+                child: IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    color: Theme.of(context).primaryIconTheme.color,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+            const Text('Hero Sample'),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Hero(
+              tag: 'container',
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push<void>(
+                    PageRouteBuilder(
+                      pageBuilder: (context, _, __) => const NextPage(),
+                      transitionDuration: const Duration(milliseconds: 1000),
+                      reverseTransitionDuration:
+                          const Duration(milliseconds: 1000),
+                    ),
+                  );
+                },
+                child: const Text('次のページへ'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  const NextPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Next Page'),
+        leading: Hero(
+          tag: 'back-button',
+          child: Material(
+            type: MaterialType.transparency,
+            child: BackButton(
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+          ),
+          // flightShuttleBuilder: (
+          //   flightContext,
+          //   animation,
+          //   flightDirection,
+          //   fromHeroContext,
+          //   toHeroContext,
+          // ) {
+          //   return const Placeholder();
+          // },
+          // placeholderBuilder: (context, size, child) {
+          //   return const Placeholder();
+          // },
+        ),
+      ),
+      body: Hero(
+        tag: 'container',
+        child: Container(
+          color: Theme.of(context).primaryColor,
+        ),
+      ),
+    );
+  }
+}
