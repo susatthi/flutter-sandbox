069fe308a8bfc44594ce1a47a417c7fe01a73a30
susa.keyber <susa@keyber.jp>
2022-05-18 13:15:10 +0900

    🚀 feat:  hashCodeの比較を追加

--- lib/main_singleton.dart
+++ lib/main_singleton.dart
@@ -2,6 +2,11 @@ void main() {
   Alice.instance.say(); // 'My name is Alice.'
   Bob.instance.say(); // 'My name is Bob.'
 
+  // Singleton なので同じインスタンスになる
+  final alice1 = Alice.instance;
+  final alice2 = Alice.instance;
+  print(alice1.hashCode == alice2.hashCode); // true
+
   // Singleton なので次のようには呼べない
   // Alice().say();
   // Bob().say();
